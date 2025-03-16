d1052e328db426a2fa9798e3683de780c9fb6507
Daveeh1108 <rizzlykeke36756@gmail.com>
2025-03-16 15:55:09 +0100

    Update +page.svelte
    Hungary!

--- src/routes/countries/learn/hungary/+page.svelte
+++ src/routes/countries/learn/hungary/+page.svelte
@@ -1,1 +1,5 @@
-<!-- Write or paste an article with HTML & Svelte here -->
+<script>
+</script>
+
+<p class="mb-2">Hungary is a small country in the middle of EU. It's capital is Budapest that. In Budepest there are districts. Hungarian peples love the drink :). Szobszlai Dominik and Puskás Péter are the biggest footballers in Hungary! Baumgartner Zsolt is the only F1 driver in Hungary!</p>
+
